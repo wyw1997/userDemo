ca7e869ceb094d75c10a162b9af6d2ae4fb45030
wyw1997 <wyw727133926@126.com>
2019-05-26 10:54:57 +0800

    user表结构更新

--- src/main/java/com/example/demo/model/PO/base/BaseEntity.java
+++ src/main/java/com/example/demo/model/PO/base/BaseEntity.java
@@ -9,8 +9,8 @@ public class BaseEntity {
 
     private int id;
     private Date createTime;
-    private int createUserId;
     private Date updateTime;
+    private int createUserId;
     private int updateUserId;
     private int deleteFlag;//0表示什么都没有
 
